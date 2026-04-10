583e062865c8eb52afc13ee485efecdecc4b40a8
shivansh1907 <dinku.cool19@gmail.com>
2026-04-10 13:11:58 +0530

    setting the frontend folder

---
+++ frontend/prescriptoAi/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  
+
+  return (
+    <>
+
+    <h1 className='text-10xl text-blue-900'>hello</h1>
+     
+    </>
+  )
+}
+
+export default App
